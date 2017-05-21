9b9c3b82934813877d066e8c5148888cd3255d2a
Marx <domah88@gmail.com>
2017-05-21 16:01:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,144 @@
+<h1 align="center">Glixer - GitHub Pages, Gulp, Jekyll, SCSS, Bootstrap.</h1>
+<p align="center">Blog, Portfolio &amp Website framework.</p>
+
+[![Join the chat at https://gitter.im/Northscript/glixer](https://badges.gitter.im/Northscript/glixer.svg)](https://gitter.im/Northscript/glixer?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)  [![StackShare](https://img.shields.io/badge/tech-stack-0690fa.svg?style=flat)](https://stackshare.io/northscript-usergroup/glixer)
+
+## Why?
+1. EASY to use and Quick to get started.
+2. Nothing else does this.
+
+People new to web developement and working professionals often do not have the knowledge or time to know which tools they should be using or the know-how to configure them.
+
+This project is meant to be a jump start for people in both groups.
+
+## Demo
+View this jekyll theme in action [here](https://northscript.github.io/)
+
+## Built with:
+- [Bootstrap](http://getbootstrap.com/)
+- [Bourbon](http://bourbon.io/)
+- [Browser Sync](https://browsersync.io/)
+- [Font Awesome](http://fontawesome.io/)
+- [GitHub Pages](https://pages.github.com/)
+- [Gulp](http://gulpjs.com/)
+- [Jekyll](https://jekyllrb.com/)
+- [jQuery](https://jquery.com/)
+- [Node.js](https://nodejs.org/)
+- [SASS](http://sass-lang.com/)
+
+The tools listed above are included with this package and are ready to go once this package is installed.
+
+## Getting Started
+
+### C9
+
+#### Setup
+
+1. Logon to Github
+2. Fork this [repository](https://github.com/north-script/glixer)
+3. Copy your fork url e.g. `git@github.com:YOURUSERNAME/glixer`
+4. On [c9.io](https://c9.io) create a new workspace
+   * Template: Blank (Ubuntu Logo)
+   * Clone from Git: use your fork url here
+5. When you get redirected to some sort of enviroment, look at the bottom, that's your command line. Click on it and enter the following command:
+`npm install`
+   
+#### Operation
+
+To view the work:
+ 1. In the command line, run `gulp serve` - If it quits by itself then there is something wrong
+ 2. To view the result, click the `preview` button at the top and click `Preview Running Application`
+ 
+To publish your work to Github run these commands in the command line:
+ ```
+ gulp publish
+ git commit -a
+ git push origin master
+ ```
+
+### Local Machines
+
+#### System Dependencies
+
+##### Windows
+
+See WINDOWS.md for instructions on installing these dependencies. these links are download links for installers
+
+###### 64-bit
+
+ - [Git](https://www.git-scm.com/download/win)
+ - [Node.js](https://nodejs.org/dist/v7.7.3/node-v7.7.3-x64.msi)
+ - [Ruby](https://dl.bintray.com/oneclick/rubyinstaller/rubyinstaller-2.3.3-x64.exe)
+ - [Ruby Developer Kit](https://dl.bintray.com/oneclick/rubyinstaller/DevKit-mingw64-64-4.7.2-20130224-1432-sfx.exe) - See WINDOWS.md for details on installing this.
+ 
+###### 32-bit
+
+ - [Git](https://www.git-scm.com/download/win)
+ - [Node.js](https://nodejs.org/dist/v7.7.3/node-v7.7.3-x86.msi)
+ - [Ruby](https://dl.bintray.com/oneclick/rubyinstaller/rubyinstaller-2.3.3.exe)
+ - [Ruby Developer Kit](https://dl.bintray.com/oneclick/rubyinstaller/DevKit-mingw64-32-4.7.2-20130224-1151-sfx.exe) - See WINDOWS.md for details on installing this.
+
+##### Mac OSX
+ - [Node.js](https://nodejs.org/dist/v7.7.3/node-v7.7.3.pkg)
+ - [xcode](https://itunes.apple.com/us/app/xcode/id497799835?mt=12)
+
+
+##### Linux
+ - [Node.js](https://nodejs.org/en/download/package-manager)
+ - `ruby`
+ - `ruby-dev`
+ - `zlib1g-dev`
+ - `build-essential`
+ - `git`
+ 
+#### Cloning and setup
+Open a Command Prompt or Terminal window and type these commands in:
+```
+git clone https://github.com/north-script/glixer
+cd glixer
+npm install
+```
+
+### Github Pages Setup (For organization/users)
+1. Create a new repository in your user/organization
+  - Make sure that initialize this repository with a README is unchecked
+  - If you are making this for a user profile, name your repository: yourusername.github.io
+  - If you are making this for a organization, make sure that the repository is created and owned by the organization and name it: yourorganization.github.io
+2. clone this repository: `git clone https://github.com/north-script/glixer`
+3. `cd glixer`
+4. Remove .git directory (Windows: `rmdir /S .git` Mac/Linux: `rm -r -f .git`) 
+5. `git init`
+6. `git remote add origin https://github.com/user-or-organization-name/user-or-organization-name.github.io`
+7. `git commit -a -m "Initial commit"`
+8. `git push origin master`
+9. Optional: `npm install`
+
+### Github Pages Setup (For projects/repositories)
+
+1. Clone your repository
+2. `cd` into your repository
+3. clone this repository `git clone https://github.com/north-script/glixer`
+4. rename the glixer folder to docs.
+5. remove the .git (hidden) folder in your `docs` folder
+6. stage your changes and commit them.
+7. push your changes to your repository.
+8. Go to your repository settings and enable github pages from your `docs` folder
+
+## Built by
+[Mike Boardley](https://www.linkedin.com/in/boardley/)
+
+Please email me your comments & feedback.
+
+- by <a href="https://twitter.com/mikeboardley">twitter</a>
+- mail <a href="mailto:boardley@gmail.com">boardley[at]gmail.com</a>
+- via <a href="https://www.linkedin.com/in/boardley/">LinkedIn</a>
+
+
+[Samuel Brekke](https://www.linkedin.com/in/sjbrekke/)
+
+Please contact me for any questions, comments, concerns and, feedback
+
+ - by mail: [brekmister@gmail.com](mailto:brekmister@gmail.com)
+ - Github: [brekmister](https://github.com/users/brekmister)
+ 
+---
---
+++ gulpfile.js
@@ -0,0 +1,196 @@
+var gulp = require('gulp'),
+	browserSync = require('browser-sync'),
+	reload = browserSync.reload,
+	sass = require('gulp-sass'),
+	bourbon = require('bourbon'),
+	neat = require('node-neat'),
+	cleanCSS = require('gulp-clean-css'),
+	sourcemaps = require('gulp-sourcemaps'),
+	jshint = require('gulp-jshint'),
+	concat = require('gulp-concat'),
+	plumber = require('gulp-plumber'),
+	notify = require('gulp-notify'),
+	child = require('child_process'),
+	gutil = require('gulp-util'),
+	prettify = require('gulp-jsbeautifier'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	run = require('gulp-run'),
+	streamqueue  = require('streamqueue'),
+	siteDir = '.site',
+	appDir = './_src',
+     lib = './';
+
+var plumberErrorHandler = {
+			errorHandler: notify.onError({
+				title: 'Gulp',
+				message: 'Error: <%= error.message %>'
+			})
+		};
+
+var config = {
+  drafts:     !!gutil.env.drafts      // pass --drafts flag to serve drafts
+};
+
+// Copy vendor libraries from /node_modules into /vendor
+gulp.task('build:copy', function() {
+
+	// Copy Fonts To .Site Folder for Local Viewing
+	gulp.src(['node_modules/font-awesome/fonts/**/*.{ttf,woff,woff2,eof,svg}'])
+		.pipe(gulp.dest(siteDir + '/fonts/font-awesome'))
+		.pipe(gulp.dest(lib + 'fonts/font-awesome'));
+
+// Copy Fonts To .Site Folder for Local Viewing
+	gulp.src('node_modules/bootstrap-sass/assets/fonts/**/*.{ttf,woff,woff2,eof,svg}')
+		.pipe(gulp.dest(siteDir + '/fonts'))
+		.pipe(gulp.dest(lib + 'fonts'));
+
+	// Copy JavaScript For Development
+	gulp.src([
+		'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
+		'node_modules/jquery/dist/jquery.js',
+		'/node_modules/leaflet/src/Leaflet.js'])
+		.pipe(gulp.dest(appDir + '/_js/vendor'));
+
+})
+
+gulp.task('build:images', function(cb) {
+    gulp.src([appDir+'/**/*.png',
+              appDir+'/**/*.jpg',
+              appDir+'/**/*.gif'])
+         .pipe(gulp.dest(siteDir + 'img'))
+         .pipe(gulp.dest(lib + 'img'));
+});
+
+// Runs Jekyll build
+gulp.task('build:jekyll', function() {
+  var shellCommand = 'bundle exec jekyll build --config ' +
+                     'local_config.yml --incremental';
+
+  if (config.drafts) { shellCommand += ' --drafts'; };
+
+  return gulp.src(appDir)
+    .pipe(run(shellCommand))
+    .on('error', gutil.log);
+});
+
+// Jekyll Watch
+gulp.task('build:jekyll:watch', ['build:jekyll'], function(cb) {
+  browserSync.reload();
+  cb();
+});
+gulp.task('build:scripts:watch', ['build:scripts'], function(cb) {
+  browserSync.reload();
+  cb();
+});
+
+// Build Scripts
+gulp.task('build:scripts', function() {
+  return streamqueue({ objectMode: true },
+        //gulp.src(appDir + '/_js/**/*.js')
+    	gulp.src(
+    		[
+	    		appDir + '/_js/vendor/jquery.js',
+	    		appDir + '/_js/vendor/bootstrap.js',
+	    		appDir + '/_js/**/*.js',
+	    		appDir + '/_js/*.js'
+    		]
+    	)
+    )
+    .pipe(concat('site.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(siteDir + '/js'))
+    .on('error', gutil.log);
+});
+
+// Compile SCSS
+gulp.task('build:styles', function() {
+	return gulp.src(appDir + '/_sass/*.scss')
+		.pipe(plumber(plumberErrorHandler))
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+				includePaths: [].concat(bourbon.includePaths, neat.includePaths)
+		}))
+		.pipe(cleanCSS()) // Issue with Clean CSS breaking code
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(siteDir + "/css"))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('prettify', function() {
+
+	gulp.src([appDir + "/_sass/**/*.scss"])
+		.pipe(prettify({
+			debug: true,
+			indent_level: 1,
+		}))
+		.pipe(gulp.dest(appDir + '/_sass'));
+
+	gulp.src([appDir + '/*.html'])
+			.pipe(prettify({
+					debug: true,
+					indent_level: 1,
+			}))
+			.pipe(gulp.dest('./'));
+
+	gulp.src([appDir + '/_js/*.js'])
+		.pipe(prettify({
+				debug: true,
+				indent_level: 1,
+		}))
+		.pipe(gulp.dest(appDir + '/_js'));
+});
+
+gulp.task('serve', ['build:scripts', 'build:styles', 'build:images', 'build:copy', 'build:jekyll'],
+          function() {
+
+  browserSync.init({
+    server: siteDir,
+    ghostMode: false, // do not mirror clicks, reloads, etc. (performance optimization)
+    logFileChanges: true,
+    port: 8080,
+    open: true      // do not open the browser (annoying)
+  });
+
+  // Watch site settings
+  gulp.watch(['_config.yml'], ['build:jekyll:watch']);
+
+  // Watch app .scss files, changes are piped to browserSync
+  gulp.watch(appDir + '/_sass/**/*.scss', ['build:styles']);
+
+  // Watch app .js files
+  gulp.watch(appDir + '/_js/**/*.js', ['build:scripts:watch']);
+
+  // Watch Jekyll posts
+  gulp.watch([appDir + '**/*.+(md|markdown|MD)', appDir + '**/*.html'], ['build:jekyll:watch']);
+
+  // Watch Jekyll drafts if --drafts flag was passed
+  if (config.drafts) {
+    gulp.watch('./_drafts/*.+(md|markdown|MD)', ['build:jekyll:watch']);
+  }
+
+  // Watch Jekyll html files
+  gulp.watch(['**/*.html', '!.site/**/*.*'], ['build:jekyll:watch']);
+
+  // Watch Jekyll RSS feed XML file
+  gulp.watch('feed.xml', ['build:jekyll:watch']);
+
+  // Watch Jekyll data files
+  gulp.watch(appDir + './_data/**.*+(yml|yaml|csv|json)', ['build:jekyll:watch']);
+
+  // Watch Jekyll favicon.ico
+  gulp.watch(appDir + './icons/favicon.ico', ['build:jekyll:watch']);
+});
+
+gulp.task('publish', ['build:scripts', 'build:styles', 'build:images', 'build:copy'],
+     function() {
+     			// Build & Copy All Of Our Jekyll Files and leave our source files alone
+          gulp.src([appDir + '/**/*', '!'+ appDir + '/**/*.yml', '!' + appDir + '/_sass', '!' + appDir + '/_sass/**', '!' + appDir + '/_js', '!' + appDir + '/_js/**'])
+               .pipe(gulp.dest(lib));
+
+          // Build & Copy Complied CSS, FONTS & JS
+          gulp.src([siteDir + '/css/', siteDir + '/**/*.css', siteDir + '/js/', siteDir + '/**/*.js'])
+         			.pipe(gulp.dest(lib));
+     }
+);
+//https://gist.github.com/bradbergeron-us/d9f4e454f5033602ce30 - I think this is what we want.
